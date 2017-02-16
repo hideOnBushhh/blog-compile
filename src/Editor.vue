e3e183f922d9c4a836a79f2e40a1e42ecc4549a2
hideOnBushhh <hideonbushhh@foxmail.com>
2017-02-16 22:14:17 +0800

    first commit

---
+++ src/Editor.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="editor">
+  <textarea :value="input" @input="update"></textarea>
+  <div id='show' v-html="compiledMarkdown"></div>
+  <div>{{compiledMarkdown}}
+    <div>
+      <Filesystem/>
+    </div>
+</template>
+
+<script>
+import Filesystem from './components/Filesystem'
+export default {
+  data() {
+    return {
+      input: '### 在此输入Markdown代码',
+    }
+  },
+  computed: {
+    compiledMarkdown: function() {
+      return marked(this.input)
+    }
+  },
+  methods: {
+    update: function(e) {
+      this.input = e.target.value;
+    }
+  },
+  components: {
+    Filesystem
+  }
+}
+</script>
---
+++ src/components/Filesystem.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="filesystem">
+  <span>请读取文件</span>
+  <input :value="file" @change="updatefile" type='file' accept=".md" id='selefile' />
+  <span>{{file}}</span>
+
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      file: ''
+    }
+  },
+  methods: {
+    updatefile: function(e) {
+      this.file = e.target.value;
+      filePath = e.target.value;
+    }
+  }
+}
+</script>
